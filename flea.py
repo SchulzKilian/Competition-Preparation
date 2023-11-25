cbe1a968b46da4c649a217a89922e6aecd785f83
AppSupportAPI <schulz.kilian@outlook.de>
2023-11-25 11:48:17 +0100

    some fixes

--- flea.py
+++ flea.py
@@ -11,32 +11,41 @@ def isblack(x,y,S):
     
 while x != "0 0 0 0 0":
     pos = x.split()
+    check = False
     S,x,y,dx,dy= int(pos[0]), int(pos[1]), int(pos[2]), int(pos[3]), int(pos[4])
     #flea on a border
 
     if dx%S == 0 or dy%S == 0:
         if x in (0,S) or y in (0,S):
             print("The flea cannot escape from black squares.")
+            check = True
 
         elif (dx/S)%2 == 0 and (dy/S)%2== 0:
             print("The flea cannot escape from black squares.")
-
+            check = True
+    if check == False:
     #maxtries = max(S//(S-S%dx),S //(S-S%dy))
-    maxtries = max(S/(dx%S), S/(dy%S))
-    print(maxtries)
-    step = 0
-    while maxtries != 0:
-        if not isblack(x,y,S):
-            print(f"After {step} jumps the flea lands at ({x}, {y}).")
-            break
-        else:
-            x += dx
-            y += dy
-            maxtries -=1
-            step +=1
-    if maxtries ==0:
-        print("The flea cannot escape from black squares.")
-        
+        try: maxtries = max(S//(dx%S), S//(dy%S))
+        except: 
+            if dx%S == dy%S == 0:
+                maxtries = 1
+            elif dx%S == 0:
+                    maxtries = S//(dy%S)
+            elif dy%S == 0:
+                maxtries = S//(dx%S)
+        step = 0
+        while maxtries != 0:
+            if not isblack(x,y,S):
+                print(f"After {step} jumps the flea lands at ({x}, {y}).")
+                break
+            else:
+                x += dx
+                y += dy
+                maxtries -=1
+                step +=1
+        if maxtries ==0:
+            print("The flea cannot escape from black squares.")
+            
     x = input()
             
         
