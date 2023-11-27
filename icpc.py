8edf0e616c3f102b0e7ceb91e61a343f99737358
AppSupportAPI <schulz.kilian@outlook.de>
2023-11-27 15:57:41 +0100

    create matrix that contains 1 if a submatrix can create it, 0 otherwise

--- icpc.py
+++ icpc.py
@@ -3,25 +3,41 @@
 x = input().split()
 
 rows, columns = int(x[0]),int(x[1])
-oldrow = ["P","P","P","P"]
+oldrow = []
 testmatrix =[[1 for i in range(rows-1)] for k in range(columns-1)]
-for i in range(columns):
-    newrow = input().split()
+for i in range(rows):
+    newrow = list(input())
     print("Iteration number" + str(i))
     print("Old row: " + str(oldrow))
     print("New row: " + str(newrow))
-    for k in range(rows):
-        item = newrow[k]
-        if item == 0:
-            continue
-        elif item == "?":
-            continue
-        elif item == "I":
-            
-            if k >0:
+    if oldrow:
+        for k in range(1,columns-1):
+            item = newrow[k]
+            print(item)
+            if item == 0:
+                continue
+            elif item == "?":
+                continue
+            elif item == "I":
+                if k >0:
+                    testmatrix[i][k-1] = 0
+                    testmatrix[i-1][k-1]= 0
+                testmatrix[i-1][k] = 0
+            elif item == "C":
+                testmatrix[i][k]= 0
+                testmatrix[i-1][k]= 0
+            elif item == "P":
+                testmatrix[i][k]= 0
+                if k >0:
+                    testmatrix[i][k-1] = 0
+                    testmatrix[i-1][k-1]= 0
+
 
 
 
     
 
     oldrow = newrow
+
+
+print(testmatrix)
